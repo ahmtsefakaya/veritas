da7bf87b3958824f75086fe92e9afbcfb603971e
ahmtsefakaya <86207927+ahmtsefakaya@users.noreply.github.com>
2026-09-14 00:51:33 +0000

    PassportModule dogrudan UsersModule'e import edildi

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PassportModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
